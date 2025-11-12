311f45256b174f7cb369cf58a105199cec558899
Jaydip85-rathod <jayuuu855@gmail.com>
2025-11-12 22:34:41 +0530

    Update API URL for fetching random quotes

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const quoteText = document.querySelector(".quote"),
 function randomQuote() {
   quoteBtn.classList.add("loading");
   quoteBtn.innerText = "Loading Quote...";
-  fetch("http://api.quotable.io/random").then(response => response.json()).then(result => {
+  fetch("https://api.quotable.io/random").then(response => response.json()).then(result => {
     quoteText.innerText = result.content;
     authorName.innerText = result.author;
     quoteBtn.classList.remove("loading");
